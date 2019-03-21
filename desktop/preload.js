d382d7c11757d1a26f1eeae4179590f0a7ab8127
Guilherme Simoes <guilherme.rdems@gmail.com>
2019-03-21 19:31:54 +0000

    Start creating proper fsApi

--- desktop/preload.js
+++ desktop/preload.js
@@ -1,3 +1,29 @@
 const fs = require('fs');
+const path = require('path');
 
-window.fsApi = fs;
+window.fs = fs;
+window.path = path;
+
+let currentPath = '/';
+
+function buildTrack(folderName, fileName) {
+    const filePath = path.join(currentPath, folderName, fileName);
+    const file = fs.readFileSync(filePath);
+    const track = {
+        name: fileName
+    }
+    return track;
+}
+
+window.fsApi = {
+    cd: (path) => {
+        currentPath = path;
+    },
+    openFolder: (folderName) => {
+        const folderPath = path.join(currentPath, folderName);
+        const fileNames = fs.readdirSync(folderPath);
+        return fileNames.map(function(fileName) {
+            return buildTrack(folderName, fileName);
+        });
+    }
+};
--- web/js/main.js
+++ web/js/main.js
@@ -2,8 +2,16 @@ import 'aframe';
 import 'super-hands';
 import 'aframe-extras';
 
-console.log(window.fsApi);
+const baseFolder = '/tmp/musicmatch';
 
-window.fsApi.mkdir('/tmp/musicmatch/a', { recursive: true }, (err) => {
+
+// Just make sure we have some folders and files
+window.fs.mkdir(`${baseFolder}/rock`, { recursive: true }, (err) => {
   if (err) throw err;
 });
+fs.writeFileSync(`${baseFolder}/rock/example.txt`, 'exampleText');
+
+
+window.fsApi.cd(baseFolder);
+const tracks = window.fsApi.openFolder('rock');
+console.log(tracks);
