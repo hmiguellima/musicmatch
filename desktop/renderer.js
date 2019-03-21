b8a8393ff50893013c30e015ef2c58a1b8a4a373
Guilherme Simoes <guilherme.rdems@gmail.com>
2019-03-21 17:03:30 +0000

    Render web project inside electron's webview

---
+++ desktop/renderer.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+  const webview = document.querySelector('webview');
+  const indicator = document.querySelector('.indicator');
+
+  webview.addEventListener('did-stop-loading', () => {
+    indicator.classList.add('hidden');
+  });
+
+  webview.addEventListener('dom-ready', () => {
+    // webview.openDevTools();
+  });
+}
